1f0620f3f8bb14d3f1ced43c27d9ad06b777bc1e
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-12 19:59:51 +0700

    เปลี่ยน

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,75 @@
-@include('welcome');
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+
+                        <p class="app-brand-text demo text-body fw-bolder text-center">เข้าสูระบบ</p>
+
+                        <h4 class="mb-2">ระบบการเบิก-จ่ายวัสดุครุภัณฑ์</h4>
+                        <p class="mb-4">โครงการพัฒนาระบบการเบิก-จ่ายวัสดุครุภัณฑ์ อบต.บางแม่นาง อ.บางใหญ่ จ.นนทบุรี</p>
+
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
+                                    {{--  <a href="#">
+                                        <small>Forgot Password?</small>
+                                    </a> --}}
+                                </div>
+                                <div class="input-group input-group-merge">
+
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="remember-me" />
+                                    <label class="form-check-label" for="remember-me"> Remember Me </label>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                            </div>
+                        </form>
+
+                        <p class="text-center">
+                            <span>New on our platform?</span>
+                            <a href="{{ route('register') }}">
+                                <span>Create an account</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <!-- /Register -->
+            </div>
+        </div>
+    </div>
+@endsection
