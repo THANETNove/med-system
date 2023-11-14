f2eca50d38d7c0d778f83fd2c4f9a1b3a99241b4
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-14 12:09:29 +0700

    ไม่เพิ่มเองได้

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,12 +62,12 @@
                             </div>
                         </form>
 
-                        <p class="text-center">
+                        {{--  <p class="text-center">
                             <span>New on our platform?</span>
                             <a href="{{ route('register') }}">
                                 <span>Create an account</span>
                             </a>
-                        </p>
+                        </p> --}}
                     </div>
                 </div>
                 <!-- /Register -->
